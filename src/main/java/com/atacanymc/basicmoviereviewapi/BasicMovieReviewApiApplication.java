246acdfb9a92019f05cf7e3684ea12d4714889b8
Ata Can <atacanymc@gmail.com>
2023-11-12 18:56:38 +0300

    getting started

---
+++ src/main/java/com/atacanymc/basicmoviereviewapi/BasicMovieReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.atacanymc.basicmoviereviewapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicMovieReviewApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicMovieReviewApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atacanymc/basicmoviereviewapi/BasicMovieReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atacanymc.basicmoviereviewapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicMovieReviewApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
